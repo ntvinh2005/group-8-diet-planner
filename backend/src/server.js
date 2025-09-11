88683f97f843203aab4384bc904530701026bb95
zander37 <alexander.dalton@ufl.edu>
2025-09-11 11:51:02 -0400

    Added Backend File Heirarchy

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Listening On Port ${PORT}`);
+});
