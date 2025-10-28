866c41bd3f771b79c849855159bacdef3bb6da87
zander37 <alexander.dalton@ufl.edu>
2025-10-28 17:30:40 -0400

    User Routes Added

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,4 +1,21 @@
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
 
 export const protectRoute = (req, res, next) => {
-  next();
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) return res.status(401).json({ message: "Access Denied, No Token Provided"} );
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
+    req.user = decoded;
+    next();
+  } catch(err) {
+    console.error("Authentication Error:", err.message);
+    return res.status(401).json({ message: "Invalid or Expired Token." });
+  }
 };
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,32 @@
 import express from "express";
-import { signUp, getUserProfile } from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { signUp, 
+    getUserProfile, 
+    logIn,
+    addPantryItem, 
+    updatePantry, 
+    updateUser,
+    deletePantryItem, 
+    deleteUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
+// --- Public Routes ---
+
 router.post("/signup", signUp);
 router.get("/profile/:username", getUserProfile);
+router.post("/login", logIn);
+
+
+// --- Protected Routes ---
+
+// User Updates
+router.put("/:id", protectRoute, updateUser);
+router.delete("/delete", protectRoute, deleteUser);
+
+// Pantry Updates
+router.post("/:id/pantry", protectRoute, addPantryItem);
+router.patch("/:id/pantry/:ingredientId", protectRoute, updatePantry);
+router.delete("/:id/pantry/:ingredientId", protectRoute, deletePantryItem);
 
 export default router;
