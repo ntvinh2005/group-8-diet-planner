093ad6d9254f48959eb181d1cb0241c7978df46c
zander37 <alexander.dalton@ufl.edu>
2025-11-22 19:05:14 -0500

    Recipe Routes/Controllers Added

--- backend/src/routes/recipe.route.js
+++ backend/src/routes/recipe.route.js
@@ -1,6 +1,25 @@
 // Recipe Routes
 import express from "express"
-
+import { protectRoute } from "../middleware/auth.middleware";
+import { authorizeRecipePerms } from "../middleware/authRecipePerms.middleware.js";
+import { listRecipes,
+    getRecipe,
+    rateRecipe,
+    createRecipe,
+    updateRecipe,
+    deleteRecipe
+ } from "../controllers/recipe.controller.js";
+ 
 const router = express.Router();
+
+// Public Routes
+router.get("/", listRecipes);
+router.get("/:recipeID", getRecipe);
+
+// Private Routes
+router.patch("/:recipeID/numStars", protectRoute, rateRecipe);
+router.post("/create", protectRoute, authorizeRecipePerms, createRecipe);
+router.put("/:recipeID", protectRoute, authorizeRecipePerms, updateRecipe);
+router.delete("/:recipeID", protectRoute, authorizeRecipePerms, deleteRecipe);
 
 export default router;
